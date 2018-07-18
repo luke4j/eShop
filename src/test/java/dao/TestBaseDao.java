951bbe5aa9ff42072d99275829fec6d5fda27036
luke <llg6yy@163.com>
2018-07-18 15:20:24 +0800

    jetty 配置文件

--- src/test/java/dao/TestBaseDao.java
+++ src/test/java/dao/TestBaseDao.java
@@ -1,6 +1,7 @@
 package dao;
 
 
+import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.junit.Before;
 import org.junit.runner.RunWith;
@@ -17,8 +18,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Rollback(false)
 public class TestBaseDao {
+        private static Logger log  = Logger.getLogger(TestBaseDao.class) ;
+    /**
+     * 注意，dao是空数据库测试
+     */
     @Before
     public void befor(){
+        log.warn("注意，dao是空数据库测试");
         PropertyConfigurator.configure(this.getClass().getResource("/test_log4j.properties"));
     }
 
